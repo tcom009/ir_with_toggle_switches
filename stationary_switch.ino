57d3b1be97db8e3d9fec97666a3273f7d97e5612
Gerson Mayer <g.mayerduran@gmail.com>
2021-02-26 20:30:12 -0500

    Toggle, Outlet, and push button commuters done and working

--- stationary_switch.ino
+++ stationary_switch.ino
@@ -9,32 +9,17 @@ class Commuter
 {
 
 private:
+  byte push;
   byte toggle;
   byte relay;
-  int _toggleStatus;
-  int _relayStatus;
+  byte _toggleStatus;
+  byte _relayStatus;
+  byte _pushStatus;
+  bool is_open = false;
   long _irOn;
   long _irOff;
-
-public:
-  //constructor
-  Commuter(byte toggle, byte relay, long irOn, long irOff)
-  {
-    this->toggle = toggle;
-    this->relay = relay;
-    this->_irOff = irOff;
-    this->_irOn = irOn;
-    this->_relayStatus = _relayStatus;
-    this->_toggleStatus = _toggleStatus;
-    init();
-  }
-
-  void init()
-  {
-    //inits the conmuter
-    pinMode(toggle, INPUT);
-    pinMode(relay, OUTPUT);
-  }
+  unsigned long lastDebounceTime = 0;
+  unsigned long debounceDelay = 50;
 
   void printStatus()
   {
@@ -47,7 +32,6 @@ public:
   void setRelayStatus()
   {
     //Changes status negating the actual state of the relay
-
     if (_relayStatus == HIGH)
     {
       _relayStatus = LOW;
@@ -66,16 +50,79 @@ public:
   void setToggleStatus()
   {
     //changes toggle's actual statue
-    if (_toggleStatus == HIGH)
+    _toggleStatus == HIGH ? _toggleStatus = LOW : _toggleStatus = HIGH;
+  }
+
+public:
+  Commuter(byte relay, long irOn, long irOff)
+  {
+    this->relay = relay;
+    this->_irOff = irOff;
+    this->_irOn = irOn;
+    this->_relayStatus = _relayStatus;
+    init();
+  }
+
+  Commuter(byte push, bool is_open, byte relay, long irOn, long irOff)
+  {
+    this->push = push;
+    this->is_open = is_open;
+    this->relay = relay;
+    this->_irOff = irOff;
+    this->_irOn = irOn;
+    this->_relayStatus = _relayStatus;
+    this->_pushStatus = _pushStatus;
+    init();
+  }
+  Commuter(byte toggle, byte relay, long irOn, long irOff)
+  {
+    this->toggle = toggle;
+    this->_toggleStatus = _toggleStatus;
+    this->relay = relay;
+    this->_irOff = irOff;
+    this->_irOn = irOn;
+    this->_relayStatus = _relayStatus;
+    init();
+  }
+
+  String getDeviceType()
+  {
+    if (push == 0 && toggle != 0)
     {
-      _toggleStatus = LOW;
+
+      return "TOGGLE";
     }
     else
     {
-      _toggleStatus = HIGH;
+      if (push != 0 && toggle == 0)
+      {
+        return "PUSH";
+      }
+      else
+      {
+        if (push == 0 && toggle == 0 && relay != 0)
+        {
+          return "OUTLET";
+        }
+      }
     }
   }
-  //experimental IR management
+
+  void init()
+  {
+    String device = Commuter::getDeviceType();
+    if (device == "TOGGLE")
+    {
+      pinMode(toggle, INPUT);
+    }
+    if (device == "PUSH")
+    {
+      pinMode(push, INPUT);
+    }
+
+    pinMode(relay, OUTPUT);
+  }
+
   void setIr()
   {
     if (irrecv.decode())
@@ -100,6 +147,7 @@ public:
   void setCommuter()
   {
     //detects if any change has happend in toggle status
+    setIr();
     int toggleReading = digitalRead(toggle);
     if (toggleReading != _toggleStatus)
     {
@@ -108,20 +156,25 @@ public:
       setToggleStatus();
     }
   }
+
+  void printDevice()
+  {
+    String device = Commuter::getDeviceType();
+    Serial.print(device);
+  }
 };
 
 //Commuter instance recives, toggle input pin, relay output pin and ir on and off codes
-Commuter Conmutador1(4, 13, 0xFE01BF40, 0xFD02BF40);
+Commuter toggle1(4, 13, 0xFE01BF40, 0xFD02BF40);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   irrecv.enableIRIn();
-  Serial.println("---Listo para recibir ordenes---")
+  Serial.println("---Listo para recibir ordenes---");
 }
 
 void loop()
 {
-  Conmutador1.setIr();
-  Conmutador1.setCommuter();
+  toggle1.setCommuter();
 }
